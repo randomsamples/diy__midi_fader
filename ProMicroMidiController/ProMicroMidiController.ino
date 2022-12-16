e54373f70d7fe3d6db534dffe910cb9d28c59bbf
randomsamples <randomsamples@github.com>
2022-12-16 03:19:38 -0800

    redone because woopsie

--- ProMicroMidiController/ProMicroMidiController.ino
+++ ProMicroMidiController/ProMicroMidiController.ino
@@ -1,24 +1,87 @@
 /*
- * MIDIUSB_buzzer.ino
+ * ProMicroMidiController.ino
  *
- * Author: Paulo Costa
+ * Author: Jason Mackay (well, cobblled together by...)
  */ 
-
+// Main USB Midi functionality is coming frm this library
 #include <MIDIUSB.h> // https://github.com/arduino-libraries/MIDIUSB
-//#include "pitchToFrequency.h"
 
-// Motor driver
+// Next thing to add is a motor driver. We want the device to
+// respond to reciept of a cc on its channel 
 // https://www.engineersgarage.com/bidirectional-motor-with-arduino/
 
+// Board Pinout Definitions
 #define FADER_IN_PIN 8 // https://i.stack.imgur.com/SEiwb.png
+#define MOTOR_R A10 // A PWM pin that will be used to control the motor
+
+// Code constants
 #define MIDI_OUT_CH 1
 #define VOLUME_CC_NUM 7
-#define MOTOR_R A10
+#define DRIVE_THAT_FADER false // enables motorized fader control
 
+/// HISTORY ////////////////////////////////////////////////////////////
+
+// History buffer used for de-noising analog measurements
 #define HISTORY_BUFF_SIZE 10
+class HistoryBuffer {
+  public:
+    HistoryBuffer();
+    void insertValue(float value);
+    float mean();
+    void display();
+  private:
+    float _sum = 0;
+    float _buffer[HISTORY_BUFF_SIZE];
+};
 
-int LAST_FADER_READING_SENT = 0;
-float HISTORY_BUFF [HISTORY_BUFF_SIZE];
+HistoryBuffer::HistoryBuffer() {}
+
+void HistoryBuffer::insertValue(float value) {
+  float new_sum = _sum;
+  for (int i = 1; i < HISTORY_BUFF_SIZE; i++) {
+
+    if (i==1) {
+      // special case for the first item only
+      // in order to maintain our sum (and therefore our average) 
+      // inexpensively, we can ust drop the record leaving the horizon 
+      // and add the new record.
+      new_sum -= _buffer[i-1];
+    }
+
+    _buffer[i-1] = _buffer[i];
+  }  
+
+  _buffer[HISTORY_BUFF_SIZE-1] = value;
+  new_sum += value;
+
+   _sum = new_sum;
+}
+
+float HistoryBuffer::mean() {
+  return _sum / (float) HISTORY_BUFF_SIZE;
+}
+
+void HistoryBuffer::display() {
+  Serial.print("History Buffer: ");
+
+  for (int i = 0; i < HISTORY_BUFF_SIZE; i++) {
+    Serial.print("[");
+    Serial.print(_buffer[i]);
+    Serial.print("]");
+    if (i != HISTORY_BUFF_SIZE - 1) {
+      Serial.print(", ");
+    }
+  }
+
+  Serial.println("");
+}
+
+HistoryBuffer history;
+
+byte LAST_VALUE_SENT = 0;
+float target_value = 0;
+
+/// /HISTORY ////////////////////////////////////////////////////////////
 
 const char* pitch_name(byte pitch) {
   static const char* names[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
@@ -91,13 +154,12 @@ void loop() {
         rx.byte2,        //control
         rx.byte3         //value
       );
-      int pwm_value = 255 * midi_level_to_voltage(rx.byte3)/1024.0;
-      Serial.print("PWN SET: ");
-      Serial.println(pwm_value);
- //     digitalWrite(MOTOR_R, true);
-      analogWrite(MOTOR_R, 255);
+
+      // TODO: We want to control the motor to seek this value
+      // This code does not do that. Yet.
+      target_value = midi_level_to_voltage(rx.byte3);
       break;
-      
+
     default:
       Serial.print("Unhandled MIDI message: ");
       Serial.print(rx.header, HEX);
@@ -110,44 +172,62 @@ void loop() {
   }
 
   float fader_voltage = analogRead(FADER_IN_PIN);
-  float midi_volume = voltage_to_midi_level(fader_voltage);
-  
-  float sum = 0;
-  for (int i = 1; i < HISTORY_BUFF_SIZE; i++) {
-    float value = HISTORY_BUFF[i];    
-    HISTORY_BUFF[i-1] = value;
-    sum += value;
-  }
+  float midi_fractional = voltage_to_midi_level(fader_voltage);
+  history.insertValue(midi_fractional);
 
-  HISTORY_BUFF[HISTORY_BUFF_SIZE - 1] = midi_volume;
+  // We stashed full res floats into the history buffer.
+  // For all outputs now we will convert to integral
+  byte midi_value = (byte)round(midi_fractional);
 
-  sum += midi_volume;
-  float average = sum / HISTORY_BUFF_SIZE;
-
-  if (abs(average - LAST_FADER_READING_SENT) >= 1) {
-    Serial.print("Sending cc: ");
-    Serial.print(VOLUME_CC_NUM);
-    Serial.print(" - ");
-    Serial.println(midi_volume);
-
-    // Serial.print("last:");
-    // Serial.println(LAST_FADER_READING_SENT);
-
-    midiEventPacket_t event = {0x0B, 0xB0 | MIDI_OUT_CH, VOLUME_CC_NUM, midi_volume};
+  float value_delta = history.mean() - (float) LAST_VALUE_SENT;  
+  // Serial.print("delta:");
+  // Serial.println(delta);
+  if (abs(value_delta) >= 1.0) {
+    // send out a cc
+    midiEventPacket_t event = {0x0B, 0xB0 | MIDI_OUT_CH, VOLUME_CC_NUM, midi_value};
     MidiUSB.sendMIDI(event);
-
     MidiUSB.flush();
+    
+    Serial.print("raw_voltage: ");
+    Serial.println(fader_voltage);
+    Serial.print("CC sent : "); // Not seeing 127 easily enough, wondering if we can fix analog ref...
+    Serial.println(midi_value);
 
-    LAST_FADER_READING_SENT = midi_volume;
+    LAST_VALUE_SENT = midi_value;
+    target_value = fader_voltage; // manual send should override any prior received pos values
   } else {
-    // Serial.println("Holding");
+    // // Do nothing
+    // Serial.print("mean: ");
+    // Serial.print(history.mean());
+    // Serial.print(", reading: ");
+    // Serial.println(midi_fractional);
+  } 
+
+  Serial.flush();
+
+  if (DRIVE_THAT_FADER) {
+    // Driving the fader
+    float fader_pos_delta = target_value - analogRead(FADER_IN_PIN);
+    while (abs(fader_pos_delta) > (0.01 * 1023)) {
+      Serial.print("delta: ");
+      Serial.println(fader_pos_delta);
+
+      if (fader_pos_delta > 0) {
+        Serial.println("Higher"); // Just a prompt for human intervention :)
+      } else {
+        Serial.println("Lower"); // for now!
+        // Make Robots do work here!
+      }
+
+      fader_pos_delta = target_value - analogRead(FADER_IN_PIN);
+    }
   }
 }
 
 float voltage_to_midi_level(float voltage) {
-  return (voltage / 1024.0) * 127.0;
+  return (voltage / 1023.0) * 127.0;
 }
 
 float midi_level_to_voltage(byte midi_level) {
-  return (midi_level/127.0) * 1024;
+  return (midi_level/127.0) * 1023.0;
 }
