ea87f387d145cbb216c025f9e9c48019e927b265
randomsamples <randomsamples@github.com>
2022-12-15 22:37:44 -0800

    initial commit, usb midi and fader sensing working

---
+++ ProMicroMidiController/ProMicroMidiController.ino
@@ -0,0 +1,153 @@
+/*
+ * MIDIUSB_buzzer.ino
+ *
+ * Author: Paulo Costa
+ */ 
+
+#include <MIDIUSB.h> // https://github.com/arduino-libraries/MIDIUSB
+//#include "pitchToFrequency.h"
+
+// Motor driver
+// https://www.engineersgarage.com/bidirectional-motor-with-arduino/
+
+#define FADER_IN_PIN 8 // https://i.stack.imgur.com/SEiwb.png
+#define MIDI_OUT_CH 1
+#define VOLUME_CC_NUM 7
+#define MOTOR_R A10
+
+#define HISTORY_BUFF_SIZE 10
+
+int LAST_FADER_READING_SENT = 0;
+float HISTORY_BUFF [HISTORY_BUFF_SIZE];
+
+const char* pitch_name(byte pitch) {
+  static const char* names[] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+  return names[pitch % 12];
+}
+
+int pitch_octave(byte pitch) {
+  return (pitch / 12) - 1;
+}
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  Serial.print("Note On: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  Serial.print("Note Off: ");
+  Serial.print(pitch_name(pitch));
+  Serial.print(pitch_octave(pitch));
+  Serial.print(", channel=");
+  Serial.print(channel);
+  Serial.print(", velocity=");
+  Serial.println(velocity);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  Serial.print("Control change: control=");
+  Serial.print(control);
+  Serial.print(", value=");
+  Serial.print(value);
+  Serial.print(", channel=");
+  Serial.println(channel);
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(MOTOR_R, OUTPUT);
+}
+
+void loop() {
+  midiEventPacket_t rx = MidiUSB.read();
+  switch (rx.header) {
+    case 0:
+      break; //No pending events
+      
+    case 0x9:
+      noteOn(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+      break;
+      
+    case 0x8:
+      noteOff(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //pitch
+        rx.byte3         //velocity
+      );
+      break;
+      
+    case 0xB:
+      controlChange(
+        rx.byte1 & 0xF,  //channel
+        rx.byte2,        //control
+        rx.byte3         //value
+      );
+      int pwm_value = 255 * midi_level_to_voltage(rx.byte3)/1024.0;
+      Serial.print("PWN SET: ");
+      Serial.println(pwm_value);
+ //     digitalWrite(MOTOR_R, true);
+      analogWrite(MOTOR_R, 255);
+      break;
+      
+    default:
+      Serial.print("Unhandled MIDI message: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+  }
+
+  float fader_voltage = analogRead(FADER_IN_PIN);
+  float midi_volume = voltage_to_midi_level(fader_voltage);
+  
+  float sum = 0;
+  for (int i = 1; i < HISTORY_BUFF_SIZE; i++) {
+    float value = HISTORY_BUFF[i];    
+    HISTORY_BUFF[i-1] = value;
+    sum += value;
+  }
+
+  HISTORY_BUFF[HISTORY_BUFF_SIZE - 1] = midi_volume;
+
+  sum += midi_volume;
+  float average = sum / HISTORY_BUFF_SIZE;
+
+  if (abs(average - LAST_FADER_READING_SENT) >= 1) {
+    Serial.print("Sending cc: ");
+    Serial.print(VOLUME_CC_NUM);
+    Serial.print(" - ");
+    Serial.println(midi_volume);
+
+    // Serial.print("last:");
+    // Serial.println(LAST_FADER_READING_SENT);
+
+    midiEventPacket_t event = {0x0B, 0xB0 | MIDI_OUT_CH, VOLUME_CC_NUM, midi_volume};
+    MidiUSB.sendMIDI(event);
+
+    MidiUSB.flush();
+
+    LAST_FADER_READING_SENT = midi_volume;
+  } else {
+    // Serial.println("Holding");
+  }
+}
+
+float voltage_to_midi_level(float voltage) {
+  return (voltage / 1024.0) * 127.0;
+}
+
+float midi_level_to_voltage(byte midi_level) {
+  return (midi_level/127.0) * 1024;
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+MIDI Fader Project
+
+# Partslist
+
+- Bit-C Pro Micro Arduino board 
+- Headers
+- Second arduino for flashing the Bit-C
+- 
+
+# Working with Pro Micro
+
+## Solder on headers
+
+The pro micro does not come with headers. Buy some and solder them on. 
+
+## Flash to use Caterina bootloader
+
+Install Arduino IDE 2.0. Add the SparkFun AVR boards support. If you have problems adding board support you may need to start over with Arduino IDE.
+
+### how to fully start over with arduino ide
+
+You will need to flash the bootloader to use Arduino IDE. Follow [these](`https://github.com/nullbitsco/docs/blob/main/bit-c/user_guide_en.md`)  instructions.
+
+## Upload the MIDI Fader project
+Git pull this repo. Open in Arduino IDE 2.0
